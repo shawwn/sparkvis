2154d42a5e6a33d3aacfb60f8bbf6aab7fc52a9a
Shawn Presser <shawnpresser@gmail.com>
2021-05-13 00:08:01 -0500

    Initial working version

--- sparkvis/__init__.py
+++ sparkvis/__init__.py
@@ -1,1 +1,65 @@
 __version__ = '0.1.0'
+
+import numpy as np
+from sparklines import sparklines
+
+
+def flatten(x):
+    if isinstance(x, (list, tuple)):
+        l = []
+        for ele in x:
+            l.extend(flatten(ele))
+        return l
+    else:
+        return [x]
+
+
+def flatlist(x):
+    if not isinstance(x, (list, tuple)):
+        x = [x]
+    return flatten(x)
+
+
+def to_numpy(x, concat_axis=-1) -> np.ndarray:
+    x = flatlist(x)
+    x = [v.numpy() if hasattr(v, 'numpy') else v for v in x]
+    # reshape to 2D.
+    x = [v.reshape([-1, v.shape[-1]]) for v in x]
+    x = np.concatenate(x, axis=concat_axis)
+    return x
+
+
+class Sparkvis:
+    value: np.ndarray
+
+    def __init__(self, value):
+        self.value = to_numpy(value)
+
+    def min(self):
+        return self.value.min()
+
+    def max(self):
+        return self.value.max()
+
+    def get_lines(self):
+        lo, hi = self.min(), self.max()
+        value = self.value - self.min()
+        return [sparklines(x, minimum=lo, maximum=hi) for x in value]
+
+    def print(self, aspect_ratio=3):
+        print('')
+        for sparklines in self.get_lines():
+            for sparkline in sparklines:
+                final = ''.join([''.join([c] * aspect_ratio) for c in sparkline])
+                print(final)
+        h, w = list(self.value.shape)
+        print(f'{h}x{w} min={self.min():.6} max={self.max():.6}')
+
+    def __call__(self):
+        self.print()
+
+
+def sparkvis(*values, aspect_ratio=3) -> Sparkvis:
+    v = Sparkvis(values)
+    v.print(aspect_ratio=aspect_ratio)
+    return v
--- tests/test_sparkvis.py
+++ tests/test_sparkvis.py
@@ -1,5 +1,106 @@
-from sparkvis import __version__
-
+import pytest
+import sparkvis
+from functools import partial
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert sparkvis.__version__ == '0.1.0'
+
+
+class API:
+    def __getattr__(self, name: str):
+        if name in self.__dict__:
+            return self[name]
+        else:
+            return getattr(self.lib, name)
+
+
+import numpy as np
+
+
+class NumpyAPI(API):
+    lib: np
+
+    def __init__(self):
+        super().__init__()
+        self.lib = np
+
+    def rand(self, *shape):
+        return self.lib.random.rand(*shape)
+
+    def cat(self, *values, dim=0):
+        return self.lib.concatenate(sparkvis.flatlist(values), axis=dim)
+
+
+try:
+    import torch
+except ImportError:
+    torch = None
+
+
+class TorchAPI(API):
+    lib: torch
+
+    def __init__(self):
+        super().__init__()
+        self.lib = torch
+
+
+try:
+    import tensorflow as tf2
+    tf = tf2.compat.v1
+except ImportError:
+    tf2 = None
+
+
+class TensorflowAPI(API):
+    lib: tf
+
+    def __init__(self):
+        super().__init__()
+        self.lib = tf
+
+    def rand(self, *shape):
+        return self.lib.random.uniform(shape)
+
+    def cat(self, *values, dim=0):
+        return self.lib.concat(sparkvis.flatlist(values), axis=dim)
+
+
+class TestAPI:
+    def __new__(cls, name):
+        print(f'TestAPI({name!r})')
+        if name not in "torch numpy tensorflow".split():
+            raise ValueError("Unexpected name")
+        if name == "torch" and torch is not None:
+            return TorchAPI()
+        if name == "tensorflow" and tf2 is not None:
+            return TensorflowAPI()
+        return NumpyAPI()
+
+
+apis = [
+    partial(TestAPI, "torch"),
+    partial(TestAPI, "tensorflow"),
+    partial(TestAPI, "numpy"),
+]
+
+
+@pytest.mark.parametrize("api", apis)
+def test_sparkvis(api):
+    lib = api()
+    x = lib.rand(7, 7)
+    v = sparkvis.Sparkvis(x)
+    assert 0 <= v.min() <= 1
+    assert 0 <= v.max() <= 1
+    sparkvis.sparkvis(lib.ones_like(x), x, lib.zeros_like(x))
+    print(x.shape)
+    x = lib.cat([lib.ones_like(x), x, lib.zeros_like(x)], dim=-1)
+    sparkvis.sparkvis(lib.ones_like(x), x, lib.zeros_like(x))
+    print(x.shape)
+    x = lib.rand(3,7,7)
+    sparkvis.sparkvis(lib.ones_like(x), x, lib.zeros_like(x))
+    print(x.shape)
+    x = lib.rand(7,7, 3)
+    sparkvis.sparkvis(lib.ones_like(x), x, lib.zeros_like(x))
+    print(x.shape)
+
